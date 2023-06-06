d32b384d023ad12a97937c94f05489dea71c117b
thamisrissi <54963641+thamisrissi@users.noreply.github.com>
2023-06-05 22:17:19 -0300

    [REFACTOR]Componentizando Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Footer = () =>{
+    <footer className="page-footer font-small blue px-sm-6">
+    <div className="footer-copyright text-center py-3">© 2022 Copyright:
+      <a>Floricultura Rissi</a>
+    </div>
+  </footer>
+}
+export default Footer
