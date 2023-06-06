3a5c5f7dec5efc677469042eb2e470f095eb547f
thamisrissi <54963641+thamisrissi@users.noreply.github.com>
2023-06-05 22:14:27 -0300

    [REFACTOR]Componentizando Sobre Nós

---
+++ src/components/SobreNos/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const SobreNos =() =>{
+    <div>
+         <section id="menu2" className="tab-pane fade pt-4">
+      <h3>Sobre Nós</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+      {/* <img src="images/equipe.jpg" class="img-thumbnail">  */}
+    </section>
+    </div>
+}
+export default SobreNos
